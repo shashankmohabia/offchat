ac65e29227fde28936cc5534ebe8387c83b3808c
Shashank Mohabia <shashankmohabia27@gmail.com>
2019-02-10 02:07:30 +0530

    add intentFilters

--- app/src/main/java/com/example/offchat/MainActivity.kt
+++ app/src/main/java/com/example/offchat/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.offchat
 
 import android.content.Context
+import android.content.IntentFilter
 import android.net.wifi.WifiManager
 import android.net.wifi.p2p.WifiP2pManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var wifiManager: WifiManager
     lateinit var wifiP2pManager: WifiP2pManager
     lateinit var wifiP2pChannel: WifiP2pManager.Channel
+    lateinit var intentFilter:IntentFilter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,5 +43,11 @@ class MainActivity : AppCompatActivity() {
         wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         wifiP2pManager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
         wifiP2pChannel = wifiP2pManager.initialize(this, mainLooper, null)
+        intentFilter = IntentFilter().apply {
+            addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+        }
     }
 }
