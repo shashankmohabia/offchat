7171b22670b984132fd5c9c756ac61c44946ccdc
Shashank Mohabia <shashankmohabia27@gmail.com>
2019-02-10 02:19:31 +0530

    add intent action for wifi state change

--- app/src/main/java/com/example/offchat/WifiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/example/offchat/WifiDirectBroadcastReceiver.kt
@@ -4,15 +4,27 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.wifi.p2p.WifiP2pManager
+import android.util.Log
 
-class WifiDirectBroadcastReceiver(manager:WifiP2pManager, channel:WifiP2pManager.Channel, activity: MainActivity): BroadcastReceiver() {
-    override fun onReceive(p0: Context?, p1: Intent?) {
-        if (p1 != null) {
-            when(p1.action){
-                WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION ->{}
-                WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION ->{}
-                WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION ->{}
-                WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION ->{}
+class WifiDirectBroadcastReceiver(manager: WifiP2pManager, channel: WifiP2pManager.Channel, activity: MainActivity) :
+    BroadcastReceiver() {
+    override fun onReceive(p0: Context?, intent: Intent?) {
+        if (intent != null) {
+            when (intent.action) {
+                WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                    val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                    if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                        Log.d("shashank", "Wifi On")
+                    } else {
+                        Log.d("shashank", "Wifi Off")
+                    }
+                }
+                WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                }
+                WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+                }
+                WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                }
             }
         }
     }
