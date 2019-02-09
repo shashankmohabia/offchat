3e4bd8410397913093797f1728f8ed5087827c3d
Shashank Mohabia <shashankmohabia27@gmail.com>
2019-02-10 01:47:52 +0530

    create BroadcastReceiver class

---
+++ app/src/main/java/com/example/offchat/WifiDirectBroadcastReceiver.kt
@@ -0,0 +1,11 @@
+package com.example.offchat
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class WifiDirectBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+    }
+}
