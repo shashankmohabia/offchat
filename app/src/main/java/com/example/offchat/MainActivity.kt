f8b4280d87cf3b86eba3693ce85eaff1d8cd1e7f
Shashank Mohabia <shashankmohabia27@gmail.com>
2019-02-10 01:26:13 +0530

    add wifiManager

--- app/src/main/java/com/example/offchat/MainActivity.kt
+++ app/src/main/java/com/example/offchat/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.offchat
 
+import android.content.Context
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var wifiManager:WifiManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+
+    }
+
+    private fun init() {
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
     }
 }
