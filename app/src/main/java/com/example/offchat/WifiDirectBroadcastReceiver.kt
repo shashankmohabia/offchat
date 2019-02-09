5708aa35fb2ee32921f67335a3771236ffaedaf3
Shashank Mohabia <shashankmohabia27@gmail.com>
2019-02-10 01:55:36 +0530

    add action listener for intent actions

--- app/src/main/java/com/example/offchat/WifiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/example/offchat/WifiDirectBroadcastReceiver.kt
@@ -7,5 +7,13 @@ import android.net.wifi.p2p.WifiP2pManager
 
 class WifiDirectBroadcastReceiver(manager:WifiP2pManager, channel:WifiP2pManager.Channel, activity: MainActivity): BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
+        if (p1 != null) {
+            when(p1.action){
+                WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION ->{}
+                WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION ->{}
+                WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION ->{}
+                WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION ->{}
+            }
+        }
     }
 }
