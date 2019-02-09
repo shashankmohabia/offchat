af51af7f6e3c543a135f9405282d295e7501e564
Shashank Mohabia <shashankmohabia27@gmail.com>
2019-02-10 01:54:19 +0530

    add properties to WifiBroadcastReceiver class

--- app/src/main/java/com/example/offchat/WifiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/example/offchat/WifiDirectBroadcastReceiver.kt
@@ -3,9 +3,9 @@ package com.example.offchat
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.wifi.p2p.WifiP2pManager
 
-class WifiDirectBroadcastReceiver: BroadcastReceiver() {
+class WifiDirectBroadcastReceiver(manager:WifiP2pManager, channel:WifiP2pManager.Channel, activity: MainActivity): BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
-
     }
 }
